65edf3f33183308cf67986aadfc83b59e6d88d89
Thai Nguyen <tnohtition@gmail.com>
2023-08-08 12:06:32 +0700

    Rename get_or_create_manufacturers to get_or_create_manufacturer

--- lib/sim_used_smartphone_app/inventory/manufacturer_service.ex
+++ lib/sim_used_smartphone_app/inventory/manufacturer_service.ex
@@ -2,9 +2,9 @@ defmodule SimUsedSmartphoneApp.Inventory.ManufacturerService do
   alias SimUsedSmartphoneApp.Repo
   alias SimUsedSmartphoneApp.Inventory.Manufacturer
 
-  @spec get_or_create_manufacturers(map()) ::
+  @spec get_or_create_manufacturer(map()) ::
           {:ok, Manufacturer.t()} | {:error, :not_found} | {:error, Ecto.Changeset.t()}
-  def get_or_create_manufacturers(%{"id" => id} = params) do
+  def get_or_create_manufacturer(%{"id" => id} = params) do
     if manufactor = Manufacturer |> Repo.get(id) do
       {:ok, manufactor}
     else
@@ -12,7 +12,7 @@ defmodule SimUsedSmartphoneApp.Inventory.ManufacturerService do
     end
   end
 
-  def get_or_create_manufacturers(params) do
+  def get_or_create_manufacturer(params) do
     params |> Manufacturer.changeset() |> Repo.insert()
   end
 end
