c70e201fb64b294322ad83deb6a6fa7303c2387a
Thai Nguyen <tnohtition@gmail.com>
2023-08-08 13:53:26 +0700

    Correct manufacturer name

--- lib/sim_used_smartphone_app/inventory/manufacturer.ex
+++ lib/sim_used_smartphone_app/inventory/manufacturer.ex
@@ -3,14 +3,14 @@ defmodule SimUsedSmartphoneApp.Inventory.Manufacturer do
   import Ecto.Changeset
   alias SimUsedSmartphoneApp.Inventory.Model
 
-  schema "manufactors" do
+  schema "manufacturers" do
     field(:name, :string)
     has_many(:models, Model)
     timestamps()
   end
 
-  def changeset(%__MODULE__{} = manufactor, attrs) do
-    manufactor
+  def changeset(%__MODULE__{} = manufacturer, attrs) do
+    manufacturer
     |> cast(attrs, [:name])
     |> validate_required([:name])
     |> unique_constraint(:name, name: :manufacturers_lower_name_index)
